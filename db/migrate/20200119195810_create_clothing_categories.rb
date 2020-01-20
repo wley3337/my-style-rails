1126c5c89c4a36e868661ea3ccd5e1df99bb0da9
wley3337 <wley3337@gmail.com>
2020-01-20 09:41:13 -0500

    Add clothing_categories columns

--- db/migrate/20200119195810_create_clothing_categories.rb
+++ db/migrate/20200119195810_create_clothing_categories.rb
@@ -1,7 +1,8 @@
 class CreateClothingCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :clothing_categories do |t|
-
+      t.integer :clothing_item_id
+      t.integer :category_id
       t.timestamps
     end
   end
