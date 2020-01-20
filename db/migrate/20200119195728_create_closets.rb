4ecafdb6f142c4a01282d39f68d760ef219f0260
wley3337 <wley3337@gmail.com>
2020-01-20 09:39:09 -0500

    Add closet migration columns

--- db/migrate/20200119195728_create_closets.rb
+++ db/migrate/20200119195728_create_closets.rb
@@ -1,7 +1,8 @@
 class CreateClosets < ActiveRecord::Migration[6.0]
   def change
     create_table :closets do |t|
-
+      t.integer :user_id
+      t.string :name
       t.timestamps
     end
   end
