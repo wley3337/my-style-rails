a562ecf2fba3d9ccd28ed3ee996ba96775e5d866
wley3337 <wley3337@gmail.com>
2020-01-20 09:43:13 -0500

    Add worn columns

--- db/migrate/20200119195829_create_worns.rb
+++ db/migrate/20200119195829_create_worns.rb
@@ -1,7 +1,8 @@
 class CreateWorns < ActiveRecord::Migration[6.0]
   def change
     create_table :worns do |t|
-
+      t.integer :outfit_id
+      t.datetime :date_worn
       t.timestamps
     end
   end
