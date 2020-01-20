51c38f1448f81552775a9654f7fc44cc8a48977d
wley3337 <wley3337@gmail.com>
2020-01-20 09:29:44 -0500

    Updated closet and outfit with model relationships

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -1,8 +1,5 @@
 class Closet < ApplicationRecord
     belongs_to :user
-<<<<<<< HEAD
     has_many :clothing_items
 
-=======
->>>>>>> 1818eae0fe0764c640c252d41d231d4c41519d00
 end
--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,10 +1,6 @@
 class Outfit < ApplicationRecord
     belongs_to :user
-<<<<<<< HEAD
     has_many :worns
     has_many :clothing_outfits
     has_many :clothing_itmes, through: :clothing_outfits
-=======
-
->>>>>>> 1818eae0fe0764c640c252d41d231d4c41519d00
 end
