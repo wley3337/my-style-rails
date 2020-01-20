432fd41dbfadf18894d540f0b2bcccb57d069c3f
wley3337 <wley3337@gmail.com>
2020-01-20 09:19:34 -0500

    Add relationship between closet and clothing_item

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -1,3 +1,5 @@
 class Closet < ApplicationRecord
     belongs_to :user
+    has_many :clothing_items
+
 end
--- app/models/clothing_item.rb
+++ app/models/clothing_item.rb
@@ -1,4 +1,6 @@
 class ClothingItem < ApplicationRecord
     has_many :clothing_outfits
     has_many :outfits, through: :clothing_outfits
+    belongs_to :closet
+
 end
