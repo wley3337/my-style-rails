08b535d0a3a7c295c851f21ab15f35f04a5a4c54
wley3337 <wley3337@gmail.com>
2020-01-20 09:37:06 -0500

    Add Outfit Table Migration

--- db/migrate/20200119195659_create_outfits.rb
+++ db/migrate/20200119195659_create_outfits.rb
@@ -1,7 +1,8 @@
 class CreateOutfits < ActiveRecord::Migration[6.0]
   def change
     create_table :outfits do |t|
-
+      t.string :season
+      t.integer :user_id
       t.timestamps
     end
   end
