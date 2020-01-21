6a77d625b3b65dfba82ed10764387845ac3c787f
wley3337 <wley3337@gmail.com>
2020-01-21 10:21:44 -0500

    Add clothing_tags columns

--- db/migrate/20200119195915_create_clothing_tags.rb
+++ db/migrate/20200119195915_create_clothing_tags.rb
@@ -1,7 +1,8 @@
 class CreateClothingTags < ActiveRecord::Migration[6.0]
   def change
     create_table :clothing_tags do |t|
-
+      t.integer :clothing_item_id
+      t.integer :tag_id
       t.timestamps
     end
   end
