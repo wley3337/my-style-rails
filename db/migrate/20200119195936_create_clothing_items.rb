af72d8e5454dcb097136ba6affbf1964399efd1d
wley3337 <wley3337@gmail.com>
2020-01-21 10:23:47 -0500

    Add clothing_item columns

--- db/migrate/20200119195936_create_clothing_items.rb
+++ db/migrate/20200119195936_create_clothing_items.rb
@@ -1,7 +1,9 @@
 class CreateClothingItems < ActiveRecord::Migration[6.0]
   def change
     create_table :clothing_items do |t|
-
+      t.integer :closet_id
+      t.string :title
+      t.string :img_url
       t.timestamps
     end
   end
