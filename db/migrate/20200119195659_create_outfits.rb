0091d7231c11a97a313ab97c275978be6147da15
wley3337 <wley3337@gmail.com>
2020-01-22 14:54:21 -0500

    Add casual boolean to outfits table

--- db/migrate/20200119195659_create_outfits.rb
+++ db/migrate/20200119195659_create_outfits.rb
@@ -2,6 +2,7 @@ class CreateOutfits < ActiveRecord::Migration[6.0]
   def change
     create_table :outfits do |t|
       t.string :season
+      t.boolean :casual
       t.integer :user_id
       t.timestamps
     end
