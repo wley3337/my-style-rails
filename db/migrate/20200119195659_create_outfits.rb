51731fc328d122473c77997fde01d69e8679d9fc
wley3337 <wley3337@gmail.com>
2020-01-22 15:42:39 -0500

    Remove season column from Outfits table

--- db/migrate/20200119195659_create_outfits.rb
+++ db/migrate/20200119195659_create_outfits.rb
@@ -2,7 +2,6 @@ class CreateOutfits < ActiveRecord::Migration[6.0]
   def change
     create_table :outfits do |t|
       t.string :name
-      t.string :season
       t.boolean :casual
       t.integer :user_id
       t.timestamps
