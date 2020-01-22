36362ec673f375729691b5822af598ff83f14193
wley3337 <wley3337@gmail.com>
2020-01-22 15:13:00 -0500

    Add Name column to outfits table

--- db/migrate/20200119195659_create_outfits.rb
+++ db/migrate/20200119195659_create_outfits.rb
@@ -1,6 +1,7 @@
 class CreateOutfits < ActiveRecord::Migration[6.0]
   def change
     create_table :outfits do |t|
+      t.string :name
       t.string :season
       t.boolean :casual
       t.integer :user_id
