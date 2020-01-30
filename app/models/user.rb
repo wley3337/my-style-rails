757f2ea0a41926b1371687bb238738fcda5acbb6
wley3337 <wley3337@gmail.com>
2020-01-30 10:16:10 -0500

    Add uniqueness to username case insensitve

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_many :outfits 
     has_many :closets
+    validates :username, uniqueness: {case_sensitive: false}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do 
+    @u1 = create(:user)
+  end
+
+  after(:all) do 
+    User.destroy_all
+  end
+
+  describe 'validations' do 
+    it 'does not allow duplicate usernames' do
+      duplicate_username = build(:user) 
+      valid_username = build(:user, username: "Jr")
+      expect(duplicate_username.valid?).to be(false)
+      expect(valid_username.valid?).to be(true)
+    end
+  end 
 end
